e1c11b809efef7d71cb78ffaa899fd9991036910
Idris <idrisnovandi@gmail.com>
2021-01-12 20:17:18 +0700

    fix: add version

--- lib/about_me/about_me.dart
+++ lib/about_me/about_me.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:swimming_exercise/utilities/components/builder_drawer.dart';
 import 'package:swimming_exercise/utilities/components/my_app_bar.dart';
+import 'package:swimming_exercise/utilities/helper/screen_config.dart';
+import 'package:swimming_exercise/utilities/theme/my_text_theme.dart';
 
 class AboutMeScreen extends StatefulWidget {
   @override
@@ -10,11 +12,120 @@ class AboutMeScreen extends StatefulWidget {
 class _AboutMeScreenState extends State<AboutMeScreen> {
   @override
   Widget build(BuildContext context) {
+    ScreenConfig().init(context);
+
     return Scaffold(
       appBar: MyAppBar(
         title: 'Tentang Saya',
       ),
       drawer: BuilderDrawer(),
+      body: Container(
+        padding: EdgeInsets.all(ScreenConfig.safeBlockVertical * 16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'AUTHOR',
+                  style: MyTextTheme.style(
+                    TextType.number2,
+                    ScreenConfig.textSizeMultiplier,
+                    isBold: true,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: ScreenConfig.safeBlockVertical * 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Nama:',
+                  style: MyTextTheme.style(
+                    TextType.heading2,
+                    ScreenConfig.textSizeMultiplier,
+                    isBold: true,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(
+                  width: ScreenConfig.safeBlockVertical * 8,
+                ),
+                Text(
+                  'Irfan Lutfianto',
+                  style: MyTextTheme.style(
+                    TextType.heading2,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: ScreenConfig.safeBlockVertical * 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Alamat:',
+                  style: MyTextTheme.style(
+                    TextType.heading2,
+                    ScreenConfig.textSizeMultiplier,
+                    isBold: true,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(
+                  width: ScreenConfig.safeBlockVertical * 8,
+                ),
+                Expanded(
+                  child: Text(
+                    'Jl. Sultan Agung RT 02 RW 04 Desa Ketanon Kec. Kedungwaru Kab. Tulungagung',
+                    style: MyTextTheme.style(
+                      TextType.heading2,
+                      ScreenConfig.textSizeMultiplier,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: ScreenConfig.safeBlockVertical * 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Email:',
+                  style: MyTextTheme.style(
+                    TextType.heading2,
+                    ScreenConfig.textSizeMultiplier,
+                    isBold: true,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(
+                  width: ScreenConfig.safeBlockVertical * 8,
+                ),
+                Text(
+                  'lutfianto.irfan97@gmail.com',
+                  style: MyTextTheme.style(
+                    TextType.heading2,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utilities/components/builder_drawer.dart
+++ lib/utilities/components/builder_drawer.dart
@@ -1,83 +1,118 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 import 'package:swimming_exercise/about_me/about_me.dart';
 import 'package:swimming_exercise/home/screens/home_screen.dart';
 import 'package:swimming_exercise/utilities/helper/screen_config.dart';
 import 'package:swimming_exercise/utilities/theme/my_text_theme.dart';
 
-class BuilderDrawer extends StatelessWidget {
+class BuilderDrawer extends StatefulWidget {
+  @override
+  _BuilderDrawerState createState() => _BuilderDrawerState();
+}
+
+class _BuilderDrawerState extends State<BuilderDrawer> {
+  String version = '1.0.0';
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void checkVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version = packageInfo.version;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     ScreenConfig().init(context);
 
     return Drawer(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Text(
-                'I',
-                style: MyTextTheme.style(
-                  TextType.number1,
-                  ScreenConfig.textSizeMultiplier,
-                  isBold: true,
+          Column(
+            children: [
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    'IL',
+                    style: MyTextTheme.style(
+                      TextType.number1,
+                      ScreenConfig.textSizeMultiplier,
+                      isBold: true,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
                 ),
-                textAlign: TextAlign.justify,
+                accountName: Text(
+                  'Irfan Lutfianto',
+                  style: MyTextTheme.style(
+                    TextType.body1,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                accountEmail: Text(
+                  'lutfianto.irfan97@gmail.com',
+                  style: MyTextTheme.style(
+                    TextType.body1,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
               ),
-            ),
-            accountName: Text(
-              'Irfan',
-              style: MyTextTheme.style(
-                TextType.body1,
-                ScreenConfig.textSizeMultiplier,
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (context) => HomeScreen(),
+                    ),
+                  );
+                },
+                title: Text(
+                  'BERANDA',
+                  style: MyTextTheme.style(
+                    TextType.body0,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                selectedTileColor: Colors.blue,
               ),
-              textAlign: TextAlign.justify,
-            ),
-            accountEmail: Text(
-              'Irfan@gmail.com',
-              style: MyTextTheme.style(
-                TextType.body1,
-                ScreenConfig.textSizeMultiplier,
-              ),
-              textAlign: TextAlign.justify,
-            ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (context) => AboutMeScreen(),
+                    ),
+                  );
+                },
+                title: Text(
+                  'TENTANG SAYA',
+                  style: MyTextTheme.style(
+                    TextType.body0,
+                    ScreenConfig.textSizeMultiplier,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                selectedTileColor: Colors.blue,
+              )
+            ],
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                CupertinoPageRoute(
-                  builder: (context) => HomeScreen(),
-                ),
-              );
-            },
-            title: Text(
-              'BERANDA',
+          Padding(
+            padding: EdgeInsets.all(ScreenConfig.safeBlockVertical * 16.0),
+            child: Text(
+              'v$version',
               style: MyTextTheme.style(
-                TextType.body0,
+                TextType.heading4,
                 ScreenConfig.textSizeMultiplier,
               ),
               textAlign: TextAlign.justify,
             ),
-            selectedTileColor: Colors.blue,
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                CupertinoPageRoute(
-                  builder: (context) => AboutMeScreen(),
-                ),
-              );
-            },
-            title: Text(
-              'TENTANG SAYA',
-              style: MyTextTheme.style(
-                TextType.body0,
-                ScreenConfig.textSizeMultiplier,
-              ),
-              textAlign: TextAlign.justify,
-            ),
-            selectedTileColor: Colors.blue,
           )
         ],
       ),
