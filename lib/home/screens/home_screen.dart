544108aba8210feee5f4b9da9bb3447a1944c46b
Idris <idrisnovandi@gmail.com>
2021-01-23 13:48:38 +0700

    fix: add background scaffold

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -7,12 +7,10 @@ import 'package:swimming_exercise/games/games_screen.dart';
 import 'package:swimming_exercise/home/inherited/home_screen_inherited.dart';
 import 'package:swimming_exercise/home/widgets/home_row_widget.dart';
 import 'package:swimming_exercise/safety/screens/safety_screen.dart';
-import 'package:swimming_exercise/swimwear/swim_wear_screen.dart';
 import 'package:swimming_exercise/utilities/components/builder_drawer.dart';
 import 'package:swimming_exercise/utilities/components/my_app_bar.dart';
 import 'package:swimming_exercise/utilities/constants.dart';
 import 'package:swimming_exercise/utilities/helper/screen_config.dart';
-import 'package:swimming_exercise/utilities/theme/my_text_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -25,53 +23,61 @@ class HomeScreen extends StatelessWidget {
           title: 'Teknik Dasar Renang',
         ),
         drawer: BuilderDrawer(),
-        body: ListView(
-          children: [
-            Image.asset('assets/images/beranda.png', height: 250, width: double.infinity, fit: BoxFit.cover,),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: HOME_MENUS.length,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                return HomeRowWidget(
-                  title: HOME_MENUS[index],
-                  onTap: () {
-                    if (index == 0) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => SafetyScreen(),
-                        ),
-                      );
-                    } else if (index == 1) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => BasicKnowledgeScreen(),
-                        ),
-                      );
-                    } else if (index == 2) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => SwimmingBasicBehaviourScreen(),
-                        ),
-                      );
-                    } else if (index == 3) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => BasicSkillsSwimmingScreen(),
-                        ),
-                      );
-                    } else if (index == 4) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => GamesScreen(),
-                        ),
-                      );
-                    }
-                  },
-                );
-              },
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/img_background.jpg"),
+              fit: BoxFit.cover,
             ),
-          ],
+          ),
+          child: ListView(
+            children: [
+              Image.asset('assets/images/beranda.png', height: 250, width: double.infinity, fit: BoxFit.cover,),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: HOME_MENUS.length,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return HomeRowWidget(
+                    title: HOME_MENUS[index],
+                    onTap: () {
+                      if (index == 0) {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => SafetyScreen(),
+                          ),
+                        );
+                      } else if (index == 1) {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => BasicKnowledgeScreen(),
+                          ),
+                        );
+                      } else if (index == 2) {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => SwimmingBasicBehaviourScreen(),
+                          ),
+                        );
+                      } else if (index == 3) {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => BasicSkillsSwimmingScreen(),
+                          ),
+                        );
+                      } else if (index == 4) {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => GamesScreen(),
+                          ),
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
